c04aefc64653b9f9a6628e806041b3f5b19fc903
Hany <Hanym7md90@gmail.com>
2025-09-24 09:59:21 +0300

    feat: Convert AdvancedAnimationChainScreen to StatefulWidget with animated container sizes

--- lib/features/loading/ui/advanced_animation_chain_screen.dart
+++ lib/features/loading/ui/advanced_animation_chain_screen.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
 
-class AdvancedAnimationChainScreen extends StatelessWidget {
+class AdvancedAnimationChainScreen extends StatefulWidget {
   const AdvancedAnimationChainScreen({super.key});
 
   @override
+  State<AdvancedAnimationChainScreen> createState() =>
+      _AdvancedAnimationChainScreenState();
+}
+
+class _AdvancedAnimationChainScreenState
+    extends State<AdvancedAnimationChainScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+  late final List<Animation<double>> containerAnimations;
+
+  final int numberOfContainers = 3;
+  final double baseSize = 10.0;
+  final double maxSize = 20.0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: (numberOfContainers * 400)),
+    )..repeat();
+
+    // Create staggered animations for each container
+    containerAnimations = List.generate(numberOfContainers, (index) {
+      final double start = index / numberOfContainers;
+      final double end =
+          (index / numberOfContainers) + (1 / numberOfContainers);
+      return Tween<double>(begin: baseSize, end: maxSize).animate(
+        CurvedAnimation(
+          parent: controller,
+          curve: Interval(start, end, curve: Curves.linear),
+        ),
+      );
+    });
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(numberOfContainers, (index) {
+            return AnimatedBuilder(
+              animation: containerAnimations[index],
+              builder: (context, child) {
+                return Container(
+                  width: containerAnimations[index].value,
+                  height: containerAnimations[index].value,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                );
+              },
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
